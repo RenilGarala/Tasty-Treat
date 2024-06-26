fa06f58bc6204a4164b1c138b368e12b229223e4
renil <renilgarala360004@gmail.com>
2024-06-26 22:47:18 +0530

    First Commit

--- src/components/User.js
+++ src/components/User.js
@@ -3,16 +3,17 @@ class User extends React.Component {
   //create cunstructor
   constructor(props) {
     super(props);
-    this.state = {
-      count: 0,
-    };
-    console.log("child constructur");
+   
+  }
+  async componentDidMount(){
+    const data = await fetch("https://api.github.com/users/renilgarala");
+    const json = await data.json();
+
+    console.log(json);
   }
 
   render() {
     const {name, location}= this.props;
-    //you can also destructure this:
-    const { count } = this.state;
     return (
       <div className="p-3 m-3 border-2 w-36">
         <div>{name}</div>
